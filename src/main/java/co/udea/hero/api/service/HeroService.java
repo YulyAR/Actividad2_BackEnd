2689e526316a79d8c17475a7adf78df56aa956c8
Yar <yuly.alvear@udea.edu.co>
2022-09-27 20:27:52 -0500

    Se agrego metodo en repository

--- src/main/java/co/udea/hero/api/service/HeroService.java
+++ src/main/java/co/udea/hero/api/service/HeroService.java
@@ -3,7 +3,6 @@ package co.udea.hero.api.service;
 import co.udea.hero.api.exception.BusinessException;
 import co.udea.hero.api.model.Hero;
 import co.udea.hero.api.repository.HeroRepository;
-import co.udea.hero.api.util.Messages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -19,19 +18,17 @@ public class HeroService {
     private final Logger log = LoggerFactory.getLogger(HeroService.class);
 
     private HeroRepository heroRepository;
-    private Messages messages;
 
 
-    public HeroService(HeroRepository heroRepository, Messages messages){
+    public HeroService(HeroRepository heroRepository){
         this.heroRepository = heroRepository;
-        this.messages = messages;
     }
 
     public Hero getHero(Integer id){
         Optional<Hero> optionalHero = heroRepository.findById(id);
         if(!optionalHero.isPresent()){
             log.info("No se encuentra un heroe con ID:"+id);
-            throw new BusinessException(messages.get("exception.data_not_found.hero"));
+            throw new BusinessException("exception.data_not_found.hero");
         }
         return optionalHero.get();
     }
